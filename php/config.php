41e7c4ee59717e4f30777a89d6fb87990814caec
Mateusz <63960220+MateuszSamin@users.noreply.github.com>
2022-04-02 19:58:42 +0200

    Add files via upload

---
+++ php/config.php
@@ -0,0 +1,6 @@
+<?php 
+    $conn = mysqli_connect("localhost", "id17865791_shchat", "q~1~[NusKp-xBBaV", "id17865791_chat");
+    if(!$conn){
+        echo "Database connected" . mysqli_connect_error();
+    }
+?>
